2bdca58254f9e321db89dfbd98a3f5351bd8671b
Deepika-Choudhary <66372106+Deepika-Choudhary@users.noreply.github.com>
2022-02-21 00:04:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+function printError(elemId, hintMsg) {
+    document.getElementById(elemId).innerHTML = hintMsg;
+}
+
+function validateForm() {
+
+    var name = document.contactForm.name.value;
+    var email = document.contactForm.email.value;
+    var mobile = document.contactForm.mobile.value;
+    var country = document.contactForm.country.value;
+    var pincode = document.contactForm.pincode.value;
+    var gender = document.contactForm.gender.value;
+    var hobbies = [];
+    var checkboxes = document.getElementsByName("hobbies[]");
+    for(var i=0; i < checkboxes.length; i++) {
+        if(checkboxes[i].checked) {
+            hobbies.push(checkboxes[i].value);
+        }
+    }
+    
+    var nameErr = emailErr = mobileErr = countryErr = pincodeErr = genderErr = true;
+    
+    if(name == "") {
+        printError("nameErr", "Please enter your name");
+    } else {
+        var regex = /^[a-zA-Z\s]+$/;                
+        if(regex.test(name) === false) {
+            printError("nameErr", "Please enter a valid name");
+        } else {
+            printError("nameErr", "");
+            nameErr = false;
+        }
+    }
+    
+    if(email == "") {
+        printError("emailErr", "Please enter your email address");
+    } else {
+        var regex = /^\S+@\S+\.\S+$/;
+        if(regex.test(email) === false) {
+            printError("emailErr", "Please enter a valid email address");
+        } else{
+            printError("emailErr", "");
+            emailErr = false;
+        }
+    }
+    
+    if(mobile == "") {
+        printError("mobileErr", "Please enter your mobile number");
+    } else {
+        var regex = /^[0-9]\d{9}$/;
+        if(regex.test(mobile) === false) {
+            printError("mobileErr", "Please enter a valid 10 digit mobile number");
+        } else{
+            printError("mobileErr", "");
+            mobileErr = false;
+        }
+    }
+    
+    if(country == "Select") {
+        printError("countryErr", "Please select your country");
+    } else {
+        printError("countryErr", "");
+        countryErr = false;
+    }
+
+    if(pincode == ""){
+        printError("pincodeErr","Please enter your pincode");
+    } else {
+        var regex = /^[0-9]\d{6}$/;
+        if(regex.test(pincode) === false){
+        printError("pincodeErr", "Please enter a valid 6 digit pincode number");
+    } else{
+        printError("pincodeErr", "");
+        pincodeErr=false;
+    }
+}
+    
+    if(gender == "") {
+        printError("genderErr", "Please select your gender");
+    } else {
+        printError("genderErr", "");
+        genderErr = false;
+    }
+    
+    if((nameErr || emailErr || mobileErr || countryErr || pincodeErr || genderErr) == true) {
+       return false;
+    } else {
+        var dataPreview = "You've entered the following details: \n" +
+                          "Full Name: " + name + "\n" +
+                          "Email Address: " + email + "\n" +
+                          "Mobile Number: " + mobile + "\n" +
+                          "Country: " + country + "\n" +
+                          "Pinocde: "+ pincode + "\n" +
+                          "Gender: " + gender + "\n";
+        if(hobbies.length) {
+            dataPreview += "Hobbies: " + hobbies.join(", ");
+        }
+        alert(dataPreview);
+    }
+};
